834e1300fa4e11548ee4d945c8820c894e2438ee
dcafonso <danielcarlosafonso@gmail.com>
2021-02-22 19:59:19 -0300

    Acionado model Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_202635) do
+ActiveRecord::Schema.define(version: 2021_02_22_204739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 2021_02_22_202635) do
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", default: "f", null: false
+    t.string "email", default: "f", null: false
+    t.string "password_digest", default: "f", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
