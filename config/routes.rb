3eecae8006eda929238002b04bfaea2b9cdcfbdc
dcafonso <danielcarlosafonso@gmail.com>
2021-02-22 17:45:52 -0300

    Criação do model contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :contacts
 
   root to: "static_pages#index"
+
+  get 'sobre', to: 'static_pages#about'
+  get 'contato', to: 'static_pages#contact'
 end
